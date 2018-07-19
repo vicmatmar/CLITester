7d4ff0d50a97714b27666411b51870b306aeb89c
Victor Martin <victormartin@centralite.com>
2018-07-19 17:08:30 -0500

    Done for now

---
+++ CLITester/Options.cs
@@ -0,0 +1,32 @@
+﻿using CommandLine;
+
+namespace CLITester
+{
+
+    /// <summary>
+    /// Sample1 Test: --host rad-em35x-17 --test "m25p20 test 6 \"c\"" --retry_count 5
+    /// Sample2 CmdRegxWait: -h rad-em35x-17 -c "cu cs5480_pinfo" -e "CS5480 Config Reg 2 0x10020A\nCS5480 Config Info Complete"
+    /// </summary>
+    class Options
+    {
+        [Option('h', "host", Required = true, HelpText = "Debug adapter address")]
+        public string Host { get; set; }
+
+        [Option('p', "port", Required = false, HelpText = "Debug adapter port", DefaultValue = 4900)]
+        public int Port { get; set; }
+
+        [Option('t', "test", Required = false, HelpText = "Test command to run")]
+        public string TestCmd { get; set; }
+
+        [Option('e', "exp", Required = false, HelpText = "Expected Test result")]
+        public string ExpVal { get; set; }
+
+        [Option('r', "retry_count", Required = false, HelpText = "Test retry count", DefaultValue = (uint)0)]
+        public uint RetryCount { get; set; }
+
+        [Option('c', "command", Required = false, HelpText = "Command to run")]
+        public string Cmd { get; set; }
+
+
+    }
+}
--- CLITester/Program.cs
+++ CLITester/Program.cs
@@ -1,15 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommandLine;
+using System;
 
 namespace CLITester
 {
     class Program
     {
-        static void Main(string[] args)
+        static NLog.Logger _logger = NLog.LogManager.GetCurrentClassLogger();
+        static int Main(string[] args)
         {
+            _logger.Info("App started");
+
+            var options = new Options();
+            var parser = new CommandLine.Parser(s => { s.MutuallyExclusive = false; });
+
+            var isValid = parser.ParseArguments(args, options);
+            if (!isValid)
+            {
+                string msg = CommandLine.Text.HelpText.AutoBuild(options).ToString();
+                Console.WriteLine(msg);
+                return -1;
+            }
+
+
+
+            CLITester tester = new CLITester(options.Host, options.Port);
+            tester.TestRetryCount = options.RetryCount;
+
+            try
+            {
+                if (options.TestCmd != null)
+                {
+                    _logger.Info("Run Test: " + options.TestCmd);
+                    string data = tester.Test(options.TestCmd);
+                    _logger.Info(data);
+                }
+                else if(options.Cmd != null)
+                {
+                    _logger.Info("Run Cmd: " + options.Cmd);
+                    _logger.Info("Exp Val: " + options.ExpVal);
+                    string data = tester.CmdWaitRegx(options.Cmd, options.ExpVal);
+                    _logger.Info(data);
+
+                }
+
+            }
+            catch(TestException ex)
+            {
+                _logger.Error(ex.Message);
+                return -1;
+            }
+            catch(Exception ex)
+            {
+                _logger.Error(ex);
+                return -2;
+            }
+
+            return 0;
         }
+
     }
 }
